47a785adaee66a62cead469b103f19e759ed6376
Baldwin <765160998@qq.com>
2021-10-30 00:15:44 +0800

    CCB初始化

--- src/router/index.js
+++ src/router/index.js
@@ -4,23 +4,35 @@ import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
+const routerList = [];
+function importAll(r) {
+  r.keys().forEach((key) => routerList.push(r(key).default))
+}
+
+importAll(require.context('./', false, /\.routes\.js/));
+const routes = [...routerList, { path: '/', name: 'home', component: Home }]
+
+
+/**
+ * 
+
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: () => import('../views/Home.vue')
   },
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import('../views/About.vue')
   }
 ]
+ */
+
 
 const router = new VueRouter({
+  mode: 'history',
   routes
 })
 
